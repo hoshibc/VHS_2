14bd4428413632b4b58be0634198faa0408133f8
Brendan Chen <bchenn25@gmail.com>
2024-10-19 14:32:43 -0400

    finished red left auto

--- src/routes/RedLeft.cpp
+++ src/routes/RedLeft.cpp
@@ -9,26 +9,36 @@
 void RedLeft() {
     PIDDataSet TestPara={1.5,0.1,0.15}; //initialize 1.5, 0.1, 0.15
     Clamp.set(false);
-    MoveEncoderPID(TestPara, -100, 14, 0.2, 0, true);
+    MoveEncoderPID(TestPara, -100, 14, 0.2, 0, true); 
     MoveEncoderPID(TestPara, -80, 6, 0.2, 0, true);
-    Clamp.set(true);
-    wait(150, msec);
+    Clamp.set(true); //clamp the first goal 
+    wait(100, msec);
+    TurnMaxTimePID(TestPara, 145, 0.3, true); //turn face stack 
     RunRoller(100);
-    TurnMaxTimePID(TestPara, 145, 0.3, true);
-    MoveEncoderPID(TestPara, 100, 30, 0.2, 145, true);
-    MoveEncoderPID(TestPara, -50, 8, 0.2, 145, true);
-    TurnMaxTimePID(TestPara, 128, 0.2, true);
-    MoveEncoderPID(TestPara, 100, 16, 0.2, 128, true);
-    wait(100,msec);
-    MoveEncoderPID(TestPara, -100, 34, 0.2, 128, true);
-    TurnMaxTimePID(TestPara, 87, 0.2, true);
-    MoveEncoderPID(TestPara, 100, 30, 0.2, 87, true);
-    TurnMaxTimePID(TestPara, 18, 0.4, true);
-    MoveEncoderPID(TestPara, 100, 30, 0.2, 18, true);
-    MoveEncoderPID(TestPara, 60, 15, 0.2, 18, true); 
-    MoveEncoderPID(TestPara, -100, 20, 0.2, 18, true);
-    TurnMaxTimePID(TestPara, -90, 0.5, true);
-    MoveEncoderPID(TestPara, 100, 60, 0.2, -90, true);
-
+    MoveEncoderPID(TestPara, 90, 32, 0.2, 150, true); //get first ring
+    MoveEncoderPID(TestPara, -50, 8, 0.2, 150, true); //move back 
+    TurnMaxTimePID(TestPara, 132, 0.3, true); //turn face second ring 
+    MoveEncoderPID(TestPara, 90, 14, 0.2, 132, true); //get second ring
+    wait(150,msec);
+    MoveEncoderPID(TestPara, -100, 28, 0.2, 132, true); //move back
+    TurnMaxTimePID(TestPara, 87, 0.2, true); //turn face third ring
+    MoveEncoderPID(TestPara, 100, 25, 0.2, 87, true); //get third ring 
+    TurnMaxTimePID(TestPara, 18, 0.3, true); //turn face fourth ring 
+    MoveEncoderPID(TestPara, 100, 30, 0.2, 18, true); //get corner ring 
+    MoveEncoderPID(TestPara, 60, 10, 0.2, 18, true); //slow move foward
+    MoveEncoderPID(TestPara, -100, 20, 0.2, 18, true); //move back 
+    RunRoller(0);
+    TurnMaxTimePID(TestPara, -80, 0.3, true); //turn face 5th ring
+    RunRoller(100);
+    intakeLift.set(true);
+    MoveEncoderPID(TestPara, 100, 50, 0.2, -80, true); //move to get 5th ring 
+    intakeLift.set(false);
+    wait(180,msec);
+    MoveEncoderPID(TestPara, -100, 3, 0.2, -80, true);
+    TurnMaxTimePID(TestPara, -150, 0.5, true);
+    MoveEncoderPID(TestPara, 50, 15, 0.2, -150, true);
+    wait(200,msec);
+    RunRoller(0);
+    arm.set(true);
 
 }
--- src/routes/RedRight.cpp
+++ src/routes/RedRight.cpp
@@ -15,26 +15,26 @@ void RedRight(){
     MoveEncoderPID(TestPara, -40, 5, 0.2, 0, true);
     Clamp.set(true);
     wait(100,msec);   
+    TurnMaxTimePID(TestPara, 58, 0.3, true); //turn to ge middle red ring
     RunRoller(100);//score preload 
-    TurnMaxTimePID(TestPara, 62, 0.3, true); //turn to ge middle red ring
     intakeLift.set(true);
-    MoveEncoderPID(TestPara, 100, 24, 0.2, 62, true);//get middle field ring 
+    MoveEncoderPID(TestPara, 100, 26, 0.2, 58, true);//get middle field ring 
     intakeLift.set(false); 
-    wait(200, msec);
-    MoveEncoderPID(TestPara, -100, 6, 0.2, 65, true);  //back away
-    wait(700,msec);
+    wait(300, msec);
+    MoveEncoderPID(TestPara, -100, 8, 0.2, 65, true);  //back away
+    RunRoller(0);
     TurnMaxTimePID(TestPara, -113, 0.5, true); // turn to face far right ring
+    RunRoller(100);
     MoveEncoderPID(TestPara, 100, 38, 0.3, -113, true); //move to get it
     MoveEncoderPID(TestPara, 70, 12, 0.3, -113, true);
     MoveEncoderPID(TestPara, -70, 2, 0.2, -113, true); //back away
     TurnMaxTimePID(TestPara, -25, 0.5, true); //turn to face corner
-    MoveEncoderPID(TestPara, 100, 33, 0.2, -25, true); //move to corner
-    MoveTimePID(TestPara, 30, 1, 0.3, -25, true); //move time pid function has speed revesed(negative speed for foward)
-    //MoveEncoderPID(TestPara, 50, 19, 0.2, -26, true);  
+    MoveEncoderPID(TestPara, 100, 30, 0.2, -25, true); //move to corner
+    MoveEncoderPID(TestPara, 60, 17, 0.2, -25, true); //move slowly
     MoveEncoderPID(TestPara, -80, 25, 0.2, -25, true); // back away from corner
     wait(150, msec);
     TurnMaxTimePID(TestPara, 120, 1, true); //turn to face mid
-    MoveEncoderPID(TestPara, 80, 34, 0.2, 140, true); //touch mid
-    MoveEncoderPID(TestPara, 30, 10, 0.2, 140, false);
+    RunRoller(0);
+    MoveEncoderPID(TestPara, 80, 45, 0.2, 120, true); //touch mid
     RunRoller(0);
 }
